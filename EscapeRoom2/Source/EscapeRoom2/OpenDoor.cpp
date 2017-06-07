40d9ffdcc20af6beb011a991a1575c085441c631
Reece Flowers <flowersth13@students.ecu.edu>
2017-06-07 15:16:13 -0400

    Added door functionality
    Added opendoor.h and opendoor.cpp as well as updating position reporting
    to include actual positions.

---
+++ EscapeRoom2/Source/EscapeRoom2/OpenDoor.cpp
@@ -0,0 +1,37 @@
+// Copyright Reece Flowers 2017
+
+#include "EscapeRoom2.h"
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+	AActor *Door = GetOwner();
+	FRotator NewRotation = FRotator(0.0f,-80.0f,0.0f);
+	Door->SetActorRotation(NewRotation);
+	// ...
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
--- EscapeRoom2/Source/EscapeRoom2/PositionReport.cpp
+++ EscapeRoom2/Source/EscapeRoom2/PositionReport.cpp
@@ -20,7 +20,8 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 	FString ObjectName = GetOwner()->GetName();
-	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for %s "), *ObjectName);
+	FString ObjectPos =GetOwner()->GetActorLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPos);
 	
 }
 
