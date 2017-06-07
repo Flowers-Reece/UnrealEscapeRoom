a7b6bfc3198744d844eb71df5de3a5ecd80b8a53
Reece Flowers <flowersth13@students.ecu.edu>
2017-06-06 22:31:32 -0400

    Ending the night
    Just added logging with the objects name

--- EscapeRoom2/Source/EscapeRoom2/PositionReport.cpp
+++ EscapeRoom2/Source/EscapeRoom2/PositionReport.cpp
@@ -11,7 +11,7 @@ UPositionReport::UPositionReport()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
+	
 }
 
 
@@ -19,8 +19,8 @@ UPositionReport::UPositionReport()
 void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
+	FString ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for %s "), *ObjectName);
 	
 }
 
