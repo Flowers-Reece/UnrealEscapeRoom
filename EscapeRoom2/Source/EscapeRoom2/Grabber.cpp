642188f35524654f2cdda16e5cb95e3b951aee7d
Reece Flowers <flowersth13@students.ecu.edu>
2017-07-17 16:04:09 -0400

    Added in Grabb and release functions and code refactor
    Refactored code in preperation for adding new functionality in next
    upload  Added in key bindings for grabbing objects with physics handle

--- EscapeRoom2/Source/EscapeRoom2/Grabber.cpp
+++ EscapeRoom2/Source/EscapeRoom2/Grabber.cpp
@@ -12,8 +12,6 @@ UGrabber::UGrabber()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
@@ -21,28 +19,44 @@ UGrabber::UGrabber()
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"));
-	// ...
+	///UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"));
+	/// look for attached Physics Handle
+	FindPhysicsHandleComponent();
+	///Look for attached Input Component
+	SetupInputComponent();
 	
 }
 
-
-// Called every frame
-void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UGrabber::SetupInputComponent()
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+	if (InputComponent)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Input Component Found!"))
+			///Bind the Input Action	
+			InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+		InputComponent->BindAction("Grab", IE_Released, this, &UGrabber::Release);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s has no Input Component"), *GetOwner()->GetName());
+	}
+}
+
+const FHitResult UGrabber::GetFirstPhysicsBodyInReach()
+{
 	//get player viewpoint every tick	
 	FVector PlayerViewPointLocation;
 	FRotator PlayerViewPointRotator;
-	
 
-	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerViewPointLocation,OUT PlayerViewPointRotator);
+
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerViewPointLocation, OUT PlayerViewPointRotator);
 	//UE_LOG(LogTemp, Warning, TEXT("Player Location: %s View Location: %s"), *PlayerViewPointLocation.ToString(),*PlayerViewPointRotator.ToString());
-	
+
 	FVector LineTraceEnd = PlayerViewPointLocation + (PlayerViewPointRotator.Vector() * Reach);
 	///DrawDebugLine(GetWorld(), PlayerViewPointLocation, LineTraceEnd, FColor(255, 0, 0), false, 0.f, 0.f,10.f);
 	//Setup Query Parameters
-	FCollisionQueryParams TraceParameters (FName(TEXT(" ")), false, GetOwner());
+	FCollisionQueryParams TraceParameters(FName(TEXT(" ")), false, GetOwner());
 
 	//Line trace (Ray Cast)
 	FHitResult Hit;
@@ -52,9 +66,49 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 		LineTraceEnd,
 		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
 		TraceParameters);
-	AActor* ActorHit =Hit.GetActor();
+	AActor* ActorHit = Hit.GetActor();
 	if (ActorHit) {
 		UE_LOG(LogTemp, Warning, TEXT("Player is looking at %s"), *(ActorHit->GetName()))
 	}
+	return FHitResult();
+}
+
+void UGrabber::FindPhysicsHandleComponent()
+{
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	if (PhysicsHandle)
+	{
+
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s has no Physics Handle Component"), *GetOwner()->GetName());
+	}
+}
+
+void UGrabber::Grab() {
+	UE_LOG(LogTemp, Warning, TEXT("Grab pressed"));
+	//LINE TRACE and see if we reach any actors with physics body collision channel set
+	GetFirstPhysicsBodyInReach();
+	///Try and reach any actors with physics body collision channel set
+	///If we hit something then attach a physics handle
+	//TODO attach physics handle
+
+}
+
+void UGrabber::Release() {
+	UE_LOG(LogTemp, Warning, TEXT("Grab released"));
+	//TODO release physics handle
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	//if the physics handle is attached
+		//move the object that is attached
+
+	
 }
 
--- EscapeRoom2/Source/EscapeRoom2/Grabber.h
+++ EscapeRoom2/Source/EscapeRoom2/Grabber.h
@@ -14,16 +14,29 @@ class ESCAPEROOM2_API UGrabber : public UActorComponent
 public:	
 	// Sets default values for this component's properties
 	UGrabber();
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
 private:
 	float Reach = 120.f;
 	
+	UPhysicsHandleComponent* PhysicsHandle = nullptr;
+	UInputComponent* InputComponent = nullptr;
+
+	// Ray-Cast and grab what is in reach	
+	void Grab();
+	// Called when grab is released
+	void Release();
+	
+	// Find attached Physics
+	void FindPhysicsHandleComponent();
+	//Setup (assumed) attached Input Component
+	void SetupInputComponent();
+		
+	// Return hit for first physics body in reach
+	const FHitResult GetFirstPhysicsBodyInReach();
 };
